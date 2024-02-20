9f1ec11365bdddb6f5f201666556068a0062b1a3
Aleksander Deda <adeda@microfin.al>
2024-02-20 15:15:00 +0100

    finnishing using context to display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,126 +1,28 @@
-import {useState} from 'react';
+import {useState, useContext} from 'react';
 
 import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import SelectedProject from './components/SelectedProject';
 import {ProjectContext} from './store/projects-context.jsx'
+import ProjectContextProvider from './store/projects-context.jsx'
 
 
 function App() {
-const [projectsState, setProjectsState] = useState({
-  selectedProjectId: undefined,
-  projects: [],
-  tasks: []
-})
-  
-  function handleAddTask(text) {
-    setProjectsState(prevState => {
-      const taskId = Math.random();
-      const newTask = {
-        text: text,
-        projectId: prevState.selectedProjectId,
-        id: taskId
-      };
-      return {
-        ...prevState,
-        tasks: [newTask, ...prevState.tasks]
-      }
-    })
-  }
-  function handleDeleteTask(id) {
-    setProjectsState(prevState => {
-      return {
-        ...prevState,
-        tasks: prevState.tasks.filter(
-          (task) => task.id !== id),
-      }
-    });
-  }
-  function handleSelectProject(id) {
-    setProjectsState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: id,
-      }
-    });
-  }
-  
-  function handleStartAddProject(){
-      setProjectsState(prevState => {
-        return {
-          ...prevState,
-          selectedProjectId: null,
-        }
-      });
-  }
-  
-  function handleCancelAddProject() {
-    setProjectsState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-      }
-    });
-  }
+  const {projects, selectedProjectId,} = useContext(ProjectContext);
+  console.log('selectedProjectId ',selectedProjectId)
 
-
-  function handleAddProject(projectData) {
-    setProjectsState(prevState => {
-      const newProject = {
-        ...projectData,
-        id: Math.random()
-      };
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: [...prevState.projects, newProject]
-      }
-    })
-  }
-
-  function handleDeleteProject(){
-    setProjectsState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: prevState.projects.filter(
-          (project) => project.id !== prevState.selectedProjectId),
-      }
-    });
-  }
-
-  const selectedProject = projectsState.projects.find(project => project.id ===projectsState.selectedProjectId);
-  let content = <SelectedProject/>;
-
-  if (projectsState.selectedProjectId === null){
-      content = <NewProject/>
-  } else if(projectsState.selectedProjectId === undefined){
-      content = <NoProjectSelected/>
-
-  }
-
-  const contextValue = {
-    selectedProjectId: projectsState.selectedProjectId,
-    selectedProject: selectedProject,
-    projects: projectsState.projects,
-    tasks: projectsState.tasks,
-    handleAddProject: handleAddProject,
-    handleStartAddProject: handleStartAddProject,
-    handleSelectProject: handleSelectProject,
-    handleCancelAddProject: handleCancelAddProject,
-    handleDeleteProject: handleDeleteProject,
-    handleAddTask: handleAddTask,
-    handleDeleteTask: handleDeleteTask
-  };
 
   return (
-    <ProjectContext.Provider  value={contextValue}>
+    
     <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar />
-          {content}
+      <ProjectContextProvider>
+
+          <ProjectsSidebar />
+
+      </ProjectContextProvider>
     </main>
-    </ProjectContext.Provider>
+    
   );
 }
 
--- src/store/projects-context.jsx
+++ src/store/projects-context.jsx
@@ -1,10 +1,16 @@
-import { createContext} from 'react'
+import { createContext, useState} from 'react'
+import SelectedProject from '../components/SelectedProject.jsx';
+import NewProject from '../components/NewProject.jsx';
+import NoProjectSelected from '../components/NoProjectSelected.jsx';
+import ProjectsSideBar from '../components/ProjectsSideBar.jsx';
+
 
 export const ProjectContext = createContext({
     selectedProjectId: undefined,
     selectedProject: null,
     projects: [],
     tasks: [],
+    // content: <NoProjectSelected/>,
     handleAddProject: () => {},
     handleStartAddProject: () => {},
     handleSelectProject: () => {},
@@ -15,3 +21,117 @@ export const ProjectContext = createContext({
     handleAddTask: () => {},
     handleDeleteTask: () => {},
     })
+
+
+export default function ProjectContextProvider({children}) {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+        tasks: []
+      })
+        
+        function handleAddTask(text) {
+          setProjectsState(prevState => {
+            const taskId = Math.random();
+            const newTask = {
+              text: text,
+              projectId: prevState.selectedProjectId,
+              id: taskId
+            };
+            return {
+              ...prevState,
+              tasks: [newTask, ...prevState.tasks]
+            }
+          })
+        }
+        function handleDeleteTask(id) {
+          setProjectsState(prevState => {
+            return {
+              ...prevState,
+              tasks: prevState.tasks.filter(
+                (task) => task.id !== id),
+            }
+          });
+        }
+        function handleSelectProject(id) {
+          setProjectsState(prevState => {
+            return {
+              ...prevState,
+              selectedProjectId: id,
+            }
+          });
+        }
+        
+        function handleStartAddProject(){
+            setProjectsState(prevState => {
+              return {
+                ...prevState,
+                selectedProjectId: null,
+              }
+            });
+        }
+        
+        function handleCancelAddProject() {
+          setProjectsState(prevState => {
+            return {
+              ...prevState,
+              selectedProjectId: undefined,
+            }
+          });
+        }
+      
+      
+        function handleAddProject(projectData) {
+          setProjectsState(prevState => {
+            const newProject = {
+              ...projectData,
+              id: Math.random()
+            };
+            return {
+              ...prevState,
+              selectedProjectId: undefined,
+              projects: [...prevState.projects, newProject]
+            }
+          })
+        }
+      
+        function handleDeleteProject(){
+          setProjectsState(prevState => {
+            return {
+              ...prevState,
+              selectedProjectId: undefined,
+              projects: prevState.projects.filter(
+                (project) => project.id !== prevState.selectedProjectId),
+            }
+          });
+        }
+      
+        const selectedProject = projectsState.projects.find(project => project.id ===projectsState.selectedProjectId);
+        let content = <SelectedProject/>;
+      
+        if (projectsState.selectedProjectId === null){
+            content = <NewProject/>
+        } else if(projectsState.selectedProjectId === undefined){
+            content = <NoProjectSelected/>
+      
+        }
+      
+        const contextValue = {
+          selectedProjectId: projectsState.selectedProjectId,
+          selectedProject: selectedProject,
+          projects: projectsState.projects,
+          tasks: projectsState.tasks,
+          handleAddProject: handleAddProject,
+          handleStartAddProject: handleStartAddProject,
+          handleSelectProject: handleSelectProject,
+          handleCancelAddProject: handleCancelAddProject,
+          handleDeleteProject: handleDeleteProject,
+          handleAddTask: handleAddTask,
+          handleDeleteTask: handleDeleteTask
+        };
+    return <ProjectContext.Provider value={contextValue}>
+               {children} {content}
+        
+        
+    </ProjectContext.Provider>
+}    
